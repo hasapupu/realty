1893a243245e25900acff9c0883cfb0b8326bf36
hasapupu <kornel.havasi77@gmail.com>
2026-09-15 05:06:36 +0200

    finished room1 cutscenes

--- scripts/cafe.gd
+++ scripts/cafe.gd
@@ -5,6 +5,9 @@ class_name CafeContext extends SceneContext
 @onready var loverboy = $YSort/Sprite2D13
 @onready var player:Player = $YSort/CharacterBody2D
 @onready var loverboy_npx = $NPCs/Intractable6
+@onready var barista_npc = $YSort/Sprite2D/Intractable
+@onready var love_particles = $Background/CPUParticles2D
+@onready var barrier = $NPCs/Intractable18
 
 func hide_poster():
 	overlay.visible = true
@@ -16,7 +19,10 @@ func move_lover(x,y):
 	loverboy.nav_to(x,y)
 	await loverboy.path_done
 	loverboy.get_node("AnimationPlayer").play("look_up")
-	
+	barista_npc.dialogue_path = "res://dialogues/cafe_confession.dialogue"
+	love_particles.visible = true
+	barrier.queue_free()
+
 func move_player(x,y):
 	player.in_cuts = true
 	player.anim.play("walk_up")
